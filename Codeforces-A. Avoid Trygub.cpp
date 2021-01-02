48b541a8f35d2c1ddbde4f602e4fa74057258f81
Akash9888 <62830132+Akash9888@users.noreply.github.com>
2021-01-02 12:17:01 +0600

    Create Codeforces-A. Avoid Trygub.cpp

---
+++ Codeforces-A. Avoid Trygub.cpp
@@ -0,0 +1,47 @@
+ 
+/*
+    Author Name: Akash Chanda Tushar(CSE,LU)
+*/
+#include<bits/stdc++.h>
+#define   ll            long long int
+#define   ull           unsigned long long
+#define   endl          "\n"
+#define   vi            vector<int>
+#define   vll           vector<ll>
+#define   vpi           vector<pair<int,int>>
+#define   vpll          vector<pair<ll,ll>>
+#define   pb            push_back
+#define   eb            emplace_back
+#define   all(p)        p.begin(),p.end()
+#define   F             first
+#define   S             second
+#define   mp            make_pair
+#define   MOD           1000000007
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+ 
+        vector<char>v;
+ 
+        for(int i=0;i<s.size();i++){
+ 
+            v.pb(s[i]);
+ 
+        }
+       sort(all(v));
+       for(int i=0;i<v.size();i++)cout<<v[i];
+        cout<<endl;
+ 
+    }
+ 
+return 0;
+}
+ 
