714163f8e42528aad2d4f04223343986defb89aa
Akash9888 <62830132+Akash9888@users.noreply.github.com>
2021-01-02 12:24:26 +0600

    Create Codeforces-A. Add Candies.cpp

---
+++ Codeforces-A. Add Candies.cpp
@@ -0,0 +1,36 @@
+ 
+/*
+    Author Name: Akash Chanda Tushar(CSE,LU)
+*/
+#include<bits/stdc++.h>
+#define   ll            long long int
+#define   ull           unsigned long long
+#define   endl          "\n"
+#define   vi            vector<int>
+#define   vll           vector<ll>
+#define   pb            push_back
+#define   eb            emplace_back
+#define   all(p)        p.begin(),p.end()
+#define   F             first
+#define   S             second
+#define   mp            make_pair
+#define   MOD           1000000007
+using namespace std;
+ 
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n<<endl;
+        for(int i=1;i<=n;i++)cout<<i<<" ";
+        cout<<endl;
+ 
+    }
+ 
+return 0;
+}
+ 
