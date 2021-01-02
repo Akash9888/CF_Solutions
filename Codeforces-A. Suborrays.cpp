2f5f3eccdd3d8b1cac8733ebc96493b6dfc475c0
Akash9888 <62830132+Akash9888@users.noreply.github.com>
2021-01-02 12:59:31 +0600

    Create Codeforces-A. Suborrays.cpp

---
+++ Codeforces-A. Suborrays.cpp
@@ -0,0 +1,29 @@
+ 
+#include<bits/stdc++.h>
+#define   ll            long long int
+#define   endl          "\n"
+#define   vi            vector<int>
+#define   vll           vector<ll>
+#define   pb            push_back
+#define   all(p)        p.begin(),p.end()
+#define   F             first
+#define   S             second
+#define   mp            make_pair
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++)cout<<i<<" ";
+        cout<<endl;
+ 
+    }
+ 
+ 
+return 0;
+}
+ 
