df3b3ccb7c59dc01a493df3438ee577ba658f434
Akash9888 <62830132+Akash9888@users.noreply.github.com>
2021-01-06 10:21:11 +0600

    Create Codeforces-1472-A. Cards for Friends.cpp

---
+++ Codeforces-1472-A. Cards for Friends.cpp
@@ -0,0 +1,48 @@
+ 
+/*
+    Author Name: Akash Chanda Tushar(CSE,LU)
+*/
+#include<bits/stdc++.h>
+#define   ll            long long int
+#define   ull           unsigned long long
+#define   endl          "\n"
+#define   vi            vector<int>
+#define   vll           vector<ll>
+#define   vpi           vector<pair<int,int>>
+#define   vpll          vector<pair<ll,ll>>
+#define   pb            push_back
+#define   eb            emplace_back
+#define   all(p)        p.begin(),p.end()
+#define   F             first
+#define   S             second
+#define   mp            make_pair
+#define   MOD           1000000007
+using namespace std;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b){return (a / gcd(a, b)) * b;}
+ 
+ 
+void solve(){
+        ll x,y,n,cnt=1;
+        cin>>x>>y>>n;
+ 
+                while(x%2==0 || y%2==0){
+                   if(x%2==0)x/=2;
+                   else if(y%2==0)y/=2;
+                   cnt*=2;
+                }
+ 
+                if(cnt<n)cout<<"NO"<<endl;
+                else cout<<"YES"<<endl;
+        }
+ 
+int main(){
+    ios_base::sync_with_stdio(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+return 0;
+}
+ 
